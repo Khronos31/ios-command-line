a4919c422a313911f1111fa88be24b6da05c8e66
Khronos <39792509+Khronos31@users.noreply.github.com>
2020-01-03 17:13:30 +0900

    Create template

---
+++ template/FLAGS.sh
@@ -0,0 +1,2 @@
+export LDFLAGS="-framework CoreFoundation -isysroot /usr/share/SDKs/iPhoneOS.sdk -F/System/Library/Frameworks -miphoneos-version-min=7.0 -arch arm64 -L/usr/lib -L/usr/local/lib"
+export CFLAGS="-framework CoreFoundation -isysroot /usr/share/SDKs/iPhoneOS.sdk -F/System/Library/Frameworks -miphoneos-version-min=7.0 -arch arm64 -L/usr/include -L/usr/local/include"
