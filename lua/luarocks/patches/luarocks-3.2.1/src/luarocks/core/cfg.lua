b3e4a2839fcca117ef67dd972b1fcac6b57fe967
Khronos31 <khronos3141@gmail.com>
2020-01-06 01:55:53 +0900

    fix luarocks

--- lua/luarocks/patches/luarocks-3.2.1/src/luarocks/core/cfg.lua
+++ lua/luarocks/patches/luarocks-3.2.1/src/luarocks/core/cfg.lua
@@ -445,12 +445,7 @@ local function make_defaults(lua_version, target_cpu, platforms, home)
          defaults.variables.AR = "llvm-ar"
          defaults.variables.RANLIB = "llvm-ranlib"
          defaults.web_browser = "uiopen"
-         local arch = target_cpu
-         if arch == "aarch64" then
-            arch = "arm64"
-         elseif arch == "arm" then
-            arch = "armv7"
-         end
+         local arch = util.popen_read("arch")
          local FLAGS = " -framework CoreFoundation -isysroot /usr/share/SDKs/iPhoneOS.sdk -miphoneos-version-min=7.0 -arch "..arch
          defaults.variables.LIBFLAG = defaults.variables.LIBFLAG..FLAGS.." -L/usr/lib -L/usr/local/lib"
          defaults.variables.CFLAGS = defaults.variables.CFLAGS..FLAGS.." -I/usr/include -I/usr/local/include -Wno-unused-command-line-argument"
