e3a2fc14614c87104ec3e3a27efc61879ee7474f
Khronos31 <khronos3141@gmail.com>
2020-02-03 15:13:34 +0900

    update adjusting

---
+++ adjusting/gettext.bash
@@ -0,0 +1,41 @@
+export CC=clang
+
+
+grep -lr -- '-install_name'|grep configure|xargs -n1 sed 's|\\\$rpath|@rpath|' -i
+
+
+export CFLAGS='-isysroot /usr/SDK -arch arm64 -I/usr/local/include'
+export LDFLAGS='-isysroot /usr/SDK -arch arm64 -L/usr/local/lib'
+
+./configure --build=aarch64-apple-darwin --prefix=/usr/local --enable-shared --enable-static=no&&
+make&&
+make install DESTDIR=$(pwd)/build/arm64
+
+
+make clean
+
+
+export CFLAGS='-isysroot /usr/SDK -arch armv7 -miphoneos-version-min=7.0 -I/usr/local/include'
+export LDFLAGS='-isysroot /usr/SDK -arch armv7 -miphoneos-version-min=7.0 -L/usr/local/lib'
+
+./configure --build=arm-apple-darwin --prefix=/usr/local --enable-shared --enable-static=no&&
+make&&
+make install DESTDIR=$(pwd)/build/armv7
+
+
+mkdir -p build/linked
+cp -r build/arm64/* build/linked/
+
+find build/ -type f -name '*.dylib'|
+xargs chmod +x
+find build/linked -type f|
+while read x;do
+  if lipo -info $x >/dev/null 2>&1;then
+    rm $x
+    lipo -create ${x/linked/armv7} ${x/linked/arm64} -output $x
+    if test -x $x;then
+      ldid -S/usr/share/SDKs/x.ent ${x/arm64/linked}
+    fi
+  fi
+done
+
