0d55f9bb11e0b9ac808cbb2d56c459fe89354879
Khronos31 <khronos3141@gmail.com>
2020-02-04 04:07:35 +0900

    update python3.8

--- python3.8/arm64.sh
+++ python3.8/arm64.sh
@@ -9,7 +9,8 @@ cd Python-3.8.1
 
 
 export CFLAGS='-isysroot /usr/share/SDKs/iPhoneOS.sdk -miphoneos-version-min=7.0 -arch arm64 -I/usr/local/include -Wno-implicit-function-declaration'
+export LDFLAGS='-isysroot /usr/share/SDKs/iPhoneOS.sdk -miphoneos-version-min=7.0 -arch arm64 -L/usr/local/lib'
 
 ./configure --build=aarch64-apple-darwin --prefix=/usr/local --enable-shared --disable-static --enable-loadable-sqlite-extensions --with-signal-module --enable-big-digits --enable-ipv6 --enable-unicode --enable-unicode=ucs4 --with-system-expat --with-system-ffi --without-ensurepip --with-dbmloader=gdbm ac_cv_func_sendfile=no ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no&&
-make -j3 LDFLAGS='-isysroot /usr/share/SDKs/iPhoneOS.sdk -miphoneos-version-min=7.0 -arch arm64 -L/usr/local/lib'&&
-make -j3 install DESTDIR=${CURRENTDIR}/build/arm64 LDFLAGS='-isysroot /usr/share/SDKs/iPhoneOS.sdk -miphoneos-version-min=7.0 -arch arm64 -L/usr/lib -L/usr/local/lib'
+make -j3&&
+make -j3 install DESTDIR=${CURRENTDIR}/build/arm64
